6bcf4fae08705f417b62a6d01a8ff08398ce1662
Hyuk Kim <lioxoil123@gmail.com>
2018-07-20 16:16:41 -0700

    added jest_test for server code/circleci config file

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../db/index.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/rooms/:room_id/reviews', (req, res) => {
+	db.getReviews(req.params.room_id, (err, results) => {
+		if (err) console.error(err);
+		res.status(201).send(results);
+	});
+});
+
+module.exports = app;
