0f82568437df9a03fd65b4069687ce6bd8482046
Hyuk Kim <lioxoil123@gmail.com>
2018-07-24 15:49:28 -0700

    implemented functionality for totalNumberReviews component

--- client/src/components/total_number_reviews/total_number_reviews.jsx
+++ client/src/components/total_number_reviews/total_number_reviews.jsx
@@ -3,7 +3,7 @@ import style from './total_number_reviews.css';
 const React = require('react');
 
 function TotalNumberReviews(props) {
-  return <div className="style.TotalNumberReviews">TOTAL NUMBER OF REVIEWS</div>;
+  return <div className="style.TotalNumberReviews">{props.numberOfReviews} Reviews</div>;
 }
 
 export default TotalNumberReviews;
