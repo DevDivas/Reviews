6229500e3a7f31285131181b73cfc09218256464
Hyuk Kim <lioxoil123@gmail.com>
2018-07-26 18:50:55 -0700

    added tests for AvgRatings component

---
+++ client/src/components/avg_rating/avg_rating.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const AvgRating = (props) => {
+  return (
+    <div>
+      {props.data.length !== 0
+        && Math.round((props.data.map(data => data.average)
+          .reduce((acc, cur) => acc + cur)) / props.data.length)}
+    </div>
+  );
+};
+
+export default AvgRating;
