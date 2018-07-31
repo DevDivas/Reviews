bdf0494ded44f2d5e7a85795f865f2497a55e278
Hyuk Kim <lioxoil123@gmail.com>
2018-07-30 18:05:51 -0700

    added read more button

--- client/src/components/user_reviews_entry/user_reviews_entry.jsx
+++ client/src/components/user_reviews_entry/user_reviews_entry.jsx
@@ -7,11 +7,13 @@ const React = require('react');
 class UserReviewsEntry extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { reportPopup: false, closingPopup: false };
+    this.state = { reportPopup: false, closingPopup: false, comment: '...' };
     this.dateConvert = this.dateConvert.bind(this);
     this.clickFlag = this.clickFlag.bind(this);
     this.clickExit = this.clickExit.bind(this);
     this.clickSubmit = this.clickSubmit.bind(this);
+    this.commentRender = this.commentRender.bind(this);
+    this.clickReadMore = this.clickReadMore.bind(this);
   }
 
   dateConvert(date) {
@@ -31,10 +33,37 @@ class UserReviewsEntry extends React.Component {
     this.setState({ reportPopup: false, closingPopup: true });
   }
 
+  clickReadMore(e) {
+    this.setState({ comment: e.target.value });
+    e.target.style.display = "none"
+  }
+
+  commentRender() {
+    if (this.props.review.comment.length > 300) {
+      // this.setState({ commentLength: false });
+      return (
+        <p>
+          {this.props.review.comment.substring(0, 300)}
+          <span>
+            {this.state.comment}
+          </span>
+          <button className={style.readMore} value={this.props.review.comment} type="button" onClick={this.clickReadMore}>
+          Read more
+          </button>
+        </p>
+      );
+    }
+    return (
+      <p>
+        {this.props.review.comment}
+      </p>
+    );
+  }
+
   render() {
     return (
       <div className={style.reviews}>
-      <button className={style.flag} onClick={this.clickFlag} />
+        <button className={style.flag} onClick={this.clickFlag} />
         <div className={style.picture} />
         <div className={style.name}>
           {this.props.review.name}
@@ -42,9 +71,7 @@ class UserReviewsEntry extends React.Component {
         <div className={style.date}>
           {this.dateConvert(this.props.review.date)}
         </div>
-        <p>
-          {this.props.review.comment}
-        </p>
+        {this.commentRender()}
         {this.state.reportPopup
           && <Popup exit={this.clickExit} submit={this.clickSubmit} />}
         {this.state.closingPopup
